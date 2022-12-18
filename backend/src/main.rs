6b6ecb27f0419f5beb93ed423c5360c87f66e40b
Alexander Biryukov <sanya.rnd@gmail.com>
2022-12-18 03:23:42 +0300

    use correct listen interface

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -33,7 +33,7 @@ async fn main() -> std::io::Result<()> {
                     .service(controller::get_current_user),
             )
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
